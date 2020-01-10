40c33248489c72e89fae3780880c42f215565e15
Your Name <you@example.com>
2020-01-10 11:55:53 +0000

    Finished mandatory command parsing

--- src/gram_parse.rs
+++ src/gram_parse.rs
@@ -7,29 +7,62 @@ pub enum CMDParseResult {
     FileNotFound,
     FilePathNotSpecified,
     FileFound,
+    OffsetFlagExists,
+    OffsetNotSpecified,
+    OffsetFlagNotSpecified,
     Success,
 }
 
+const GRAMMER_FILE_FLAG: &str = "-g";
+const BINARY_FILE_FLAG: &str = "-b";
+const OFFSET_FLAG: &str = "-o";
+
+pub const ERROR_START: &str = "[-] Error:";
+
 pub fn check_mandatory_cmds(cmdline_hashmap: &mut HashMap<String, Option<String>>) -> CMDParseResult {
-    let cmd_parse_result: CMDParseResult = check_cmd_and_file_exists(cmdline_hashmap,"-g");
+    let cmd_parse_result: CMDParseResult = check_flag_and_file_exists(cmdline_hashmap,GRAMMER_FILE_FLAG);
 
     if cmd_parse_result != CMDParseResult::FileFound {
         return cmd_parse_result;
     }else {
     }
 
-    let cmd_parse_result: CMDParseResult = check_cmd_and_file_exists(cmdline_hashmap,"-b");
+    let cmd_parse_result = check_flag_and_file_exists(cmdline_hashmap,BINARY_FILE_FLAG);
 
     if cmd_parse_result != CMDParseResult::FileFound {
         return cmd_parse_result;
     }else {
+    }
+
+    let cmd_parse_result = check_flag_and_value_exists(cmdline_hashmap, OFFSET_FLAG);
+
+    if cmd_parse_result != CMDParseResult::OffsetFlagExists {
+        return cmd_parse_result;
+    } else {
     }
 
     return CMDParseResult::Success;
 
 }
 
-pub fn check_cmd_and_file_exists(
+fn check_flag_and_value_exists (cmdline_hashmap: &mut HashMap<String, Option<String>>, key: &str 
+) -> CMDParseResult {
+    match cmdline_hashmap.contains_key(OFFSET_FLAG) {
+        true => match cmdline_hashmap.get(OFFSET_FLAG).unwrap() {
+            Some(entry) => return CMDParseResult::OffsetFlagExists,
+            None => {
+                eprintln!("{} You need to specify an offset into the binary file",ERROR_START);
+                return CMDParseResult::OffsetNotSpecified
+            }
+        }
+        false => {
+            eprintln!("{} You need to specify the offset flag {}",ERROR_START,OFFSET_FLAG);
+            return CMDParseResult::OffsetFlagNotSpecified
+        }
+    }
+}
+
+fn check_flag_and_file_exists(
     cmdline_hashmap: &mut HashMap<String, Option<String>>, key: &str 
 ) -> CMDParseResult {
     match cmdline_hashmap.contains_key(key) {
@@ -37,17 +70,17 @@ pub fn check_cmd_and_file_exists(
             Some(entry) => match Path::new(entry).exists() {
                 true => return CMDParseResult::FileFound,
                 false => {
-                    eprintln!("Error: Could not find file {:#?}",entry);
+                    eprintln!("{} Could not find file {:#?}",ERROR_START,entry);
                     return CMDParseResult::FileNotFound
                 }
             },
             None => {
-                eprintln!("Error: You need to specify a value for flag {}",key);
+                eprintln!("{} You need to specify a value for flag {}",ERROR_START,key);
                 return CMDParseResult::FilePathNotSpecified
             } 
         },
         false => {
-            eprintln!("Error: You need to specify the flag {}",key);
+            eprintln!("{} You need to specify the flag {}",ERROR_START,key);
             return CMDParseResult::FilePathNotSpecified
         }
     }
