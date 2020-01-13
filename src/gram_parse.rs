a4892a11b0ea53fa0f396c4b312b079fc3746f29
Your Name <you@example.com>
2020-01-13 16:27:25 +0000

    Added open grammer file

--- src/gram_parse.rs
+++ src/gram_parse.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::fs::File;
 use std::path::Path;
 
 #[derive(PartialEq)]
@@ -13,70 +12,82 @@ pub enum CMDParseResult {
     Success,
 }
 
-const GRAMMER_FILE_FLAG: &str = "-g";
-const BINARY_FILE_FLAG: &str = "-b";
-const OFFSET_FLAG: &str = "-o";
+pub const GRAMMER_FILE_FLAG: &str = "-g";
+pub const BINARY_FILE_FLAG: &str = "-b";
+pub const OFFSET_FLAG: &str = "-o";
 
 pub const ERROR_START: &str = "[-] Error:";
 
-pub fn check_mandatory_cmds(cmdline_hashmap: &mut HashMap<String, Option<String>>) -> CMDParseResult {
-
-    match check_flag_and_file_exists(cmdline_hashmap,GRAMMER_FILE_FLAG) {
+pub fn check_mandatory_cmds(
+    cmdline_hashmap: &mut HashMap<String, Option<String>>,
+) -> CMDParseResult {
+    match check_flag_and_file_exists(cmdline_hashmap, GRAMMER_FILE_FLAG) {
         CMDParseResult::FileFound => (),
-        cmd_parse_result @ _ => return cmd_parse_result
+        cmd_parse_result @ _ => return cmd_parse_result,
     }
 
     match check_flag_and_file_exists(cmdline_hashmap, BINARY_FILE_FLAG) {
         CMDParseResult::FileFound => (),
-        cmd_parse_result @ _ => return cmd_parse_result
+        cmd_parse_result @ _ => return cmd_parse_result,
     }
 
     match check_flag_and_value_exists(cmdline_hashmap, OFFSET_FLAG) {
         CMDParseResult::OffsetFlagExists => (),
-        cmd_parse_result @ _ => return cmd_parse_result
+        cmd_parse_result @ _ => return cmd_parse_result,
     }
 
     return CMDParseResult::Success;
-
 }
 
-fn check_flag_and_value_exists (cmdline_hashmap: &mut HashMap<String, Option<String>>, key: &str 
+fn check_flag_and_value_exists(
+    cmdline_hashmap: &mut HashMap<String, Option<String>>,
+    key: &str,
 ) -> CMDParseResult {
-    match cmdline_hashmap.contains_key(OFFSET_FLAG) {
-        true => match cmdline_hashmap.get(OFFSET_FLAG).unwrap() {
+    match cmdline_hashmap.contains_key(key) {
+        true => match cmdline_hashmap.get(key).unwrap() {
             Some(entry) => return CMDParseResult::OffsetFlagExists,
             None => {
-                eprintln!("{} You need to specify an offset into the binary file",ERROR_START);
-                return CMDParseResult::OffsetNotSpecified
+                eprintln!(
+                    "{} You need to specify an offset into the binary file",
+                    ERROR_START
+                );
+                return CMDParseResult::OffsetNotSpecified;
             }
-        }
+        },
         false => {
-            eprintln!("{} You need to specify the offset flag {}",ERROR_START,OFFSET_FLAG);
-            return CMDParseResult::OffsetFlagNotSpecified
+            eprintln!(
+                "{} You need to specify the offset flag {}",
+                ERROR_START, OFFSET_FLAG
+            );
+            return CMDParseResult::OffsetFlagNotSpecified;
         }
     }
 }
 
 fn check_flag_and_file_exists(
-    cmdline_hashmap: &mut HashMap<String, Option<String>>, key: &str 
+    cmdline_hashmap: &mut HashMap<String, Option<String>>,
+    key: &str,
 ) -> CMDParseResult {
     match cmdline_hashmap.contains_key(key) {
         true => match cmdline_hashmap.get(key).unwrap() {
             Some(entry) => match Path::new(entry).exists() {
                 true => return CMDParseResult::FileFound,
                 false => {
-                    eprintln!("{} Could not find file {:#?}",ERROR_START,entry);
-                    return CMDParseResult::FileNotFound
+                    eprintln!("{} Could not find file {:#?}", ERROR_START, entry);
+                    return CMDParseResult::FileNotFound;
                 }
             },
             None => {
-                eprintln!("{} You need to specify a value for flag {}",ERROR_START,key);
-                return CMDParseResult::FilePathNotSpecified
-            } 
+                eprintln!(
+                    "{} You need to specify a value for flag {}",
+                    ERROR_START, key
+                );
+                return CMDParseResult::FilePathNotSpecified;
+            }
         },
         false => {
-            eprintln!("{} You need to specify the flag {}",ERROR_START,key);
-            return CMDParseResult::FilePathNotSpecified
+            eprintln!("{} You need to specify the flag {}", ERROR_START, key);
+            return CMDParseResult::FilePathNotSpecified;
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ mod hexfmt;
 use std::collections::HashMap;
 use std::env;
 use std::process;
+use std::fs;
+use std::fs::File;
 
-fn usage_info() {
+fn usage_and_exit(exit_code: i32) {
     println!("usage: blah blah");
+    process::exit(exit_code);
 }
 
 fn main() {
@@ -15,17 +18,23 @@ fn main() {
 
     match arg_parse::parse_cmdline_args(args, &mut cmdline_hashmap) {
         arg_parse::ArgParseResult::Success => (),
-        cmdline_parse_result @ _ => {
-            usage_info();
-            process::exit(cmdline_parse_result as i32);
-        }
+        cmdline_parse_result @ _ => usage_and_exit(cmdline_parse_result as i32)
     };
 
     match gram_parse::check_mandatory_cmds(&mut cmdline_hashmap) {
         gram_parse::CMDParseResult::Success => (),
-        binary_parse_result @ _ =>  {
-            usage_info();
-            process::exit(binary_parse_result as i32);
-        }
+        binary_parse_result @ _ => usage_and_exit(binary_parse_result as i32)
     };
+
+    let gram_file = match cmdline_hashmap.get(gram_parse::GRAMMER_FILE_FLAG).unwrap() {
+        Some(path) => {
+            match fs::read_to_string(path) {
+                Ok(file) => file,
+                Err(error) => panic!("[-] Error opening file: {}",error)
+            }
+        }
+        None => panic!("[-] No value for -g flag") 
+    };
+
+
 }
