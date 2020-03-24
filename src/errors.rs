8813363e257d4f482088b12685b1e2b723a9e3cd
Yumeko <yumeko@Yumeko.local>
2020-03-24 12:50:51 +0000

    updated usage information

--- src/errors.rs
+++ src/errors.rs
@@ -13,12 +13,21 @@ macro_rules! serror {
 }
 
 const USAGE_STRING: &str = "memgram [OPTION] [VALUE]";
-const OPTIONS: [&str; 3] = [
-    "-g     grammer file",
-    "-b     binary file",
-    "-o     offset into binary file",
+const OPTIONS: [&str; 6] = [
+    "-g     grammer filepath",
+    "-b     binary filepath",
+    "-o     offset into binary file structure starts at",
+    "-e     reverse endianess for formatted data",
+    "-c     c struct filepath for conversion",
+    "-o     output filepath for conversion"
 ];
 
+const EXAMPLES: [&str; 2] = [
+    "memgram -b ./examples/test_formats.bin -g ./grammer/test_formats.toml -o 0",
+    "memgram -c ./examples/COFFHeader.h -o ./grammer/COFFHeader.toml",
+];
+
+
 pub fn usage() {
-    println!("USAGE:\n\t{}\n\nOPTIONS:{:#?}\n\nEXAMPLE:\n\tmemgram -b ./examples/test_formats.bin -g ./grammer/test_formats.toml -o 0",USAGE_STRING,OPTIONS);
+    println!("USAGE: {:}\n\nOPTIONS:\n{:#?}\n\nEXAMPLE:\n{:#?}",USAGE_STRING,OPTIONS,EXAMPLES);
 }
